fedf8a31c52cc152269a47e491131e1b5aecf67a
Michael Pereira <michaelpereira408@gmail.com>
2025-07-14 00:07:18 -0300

    Metodos bando de dados

---
+++ 04 - Banco de Dados/02 - Métodos/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := NewProduct("Notebook", 100.00)
+
+	err = insertProduct(db, product)
+
+	if err != nil {
+		panic(err)
+	}
+
+	product.Price = 2000.00
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	product, err = selectOneProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Product: %v, possui o preço de %v\n", product.Name, product.Price)
+
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+stmt, err := db.Prepare("UPDATE products SET name = ?, price = ? WHERE id = ?")
+if err != nil {
+	return err
+}
+defer stmt.Close()
+
+_, err = stmt.Exec(product.Name, product.Price, product.ID)
+if err != nil {
+	return err
+}
+
+return nil
+
+}
+
+func selectOneProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("SELECT * FROM products WHERE id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var product Product
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+
+}
